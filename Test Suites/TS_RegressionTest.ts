c6a9b92a0c8ffe94878db381f1075b57372b400b
bAraia <araia.bereket@gmail.com>
2019-10-28 15:39:06 -0400

    commit 2

--- Test Suites/TS_RegressionTest.ts
+++ Test Suites/TS_RegressionTest.ts
@@ -4,7 +4,6 @@
    <name>TS_RegressionTest</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2019-01-14T16:59:38</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
@@ -14,7 +13,7 @@
    <testCaseLink>
       <guid>12b1ed24-3d2b-4bed-87e1-7c72470cdce7</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Main Test Cases/TC1_Verify Successful Login</testCaseId>
       <variableLink>
          <testDataLinkId></testDataLinkId>
@@ -32,7 +31,163 @@
    <testCaseLink>
       <guid>e4bf2157-13c0-4c5f-875c-fa60a7e7a2fa</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Main Test Cases/TC2_Verify Successful Appointment</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d0c283d8-4248-4381-974d-c470cd1f3713</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/CENTRAL_CODER/CCQ1.1_Central_Coder_Queue_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bd6c741f-4c34-4955-8906-c025fe73fada</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/CENTRAL_CODER/CCQ1.2_CCQ_Open_Tab_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5509e5df-507b-44d6-9ed6-74a4e1b0fc8d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/CENTRAL_CODER/CCQ1.3_Central_Coder_Queue_Completed_Tab_test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0a5fd3b5-7950-4c51-800a-ad43ba8c19a5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/CENTRAL_CODER/CCQ2.1_REVIEW_SUBMISSION_DetailsTab_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>033504e5-7a10-40b1-a402-d9cc22369b68</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/CENTRAL_CODER/CCQ2.2_REVIEW_SUBMISSION_SubmissionTab_Page_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f2426b0c-c0d5-4b49-ab6d-1ba3fc7640df</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/CENTRAL_CODER/CCQ3.1_SENIOR_EVALUATION_DetailsTab_Page_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f244c167-0c82-4c52-beae-81b017a9e3c3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/CENTRAL_CODER/CCQ3.2_SENIOR_EVALUATION_EvaluateTab_Page_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4bf7418e-6e03-4b09-a910-2e12b72b23fb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/CENTRAL_CODER/CCQ4.1_INVALID_CONFIRMATION_Details_Page_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>58284853-f389-4b16-a556-1ebc961e5da2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/CENTRAL_CODER/CCQ4.2_INVALID_CONFIRMATION_Validate_Page_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>48955d49-1f54-4d82-b6ad-1382c677c8da</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/CENTRAL_CODER/CCQ5_FOCUSED_REVIEW_CodeMR_Tab_Page_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bdbd885d-50b7-46d7-8169-7e18a4b9736f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/CENTRAL_CODER/CCQ5_FOCUSED_REVIEW_DetailsTab_Page_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0fa6cd9b-7c8e-4329-a905-6354acdbbcab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/CENTRAL_CODER/CCQ6_DISCREPANT_CONFIRMATION_DetailsTab_Page_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>88b0f791-0e23-4153-b569-27f5dff15392</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/CENTRAL_CODER/CCQ6_DISCREPANT_CONFIRMATION_ValidateTab_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a5161a66-cd70-4d4d-8c8d-6b50bb65b886</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/PLAN_PORTAL/PP1_LogIn_Page_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e492dc78-5c35-4159-822d-c2566c727aeb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/PLAN_PORTAL/PP2_Plan_Portal_Page_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>730a90e3-01d9-4de5-86ca-08d7fe111057</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/PLAN_PORTAL/PP3.1_List_of_Enrollees_page_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>daa5e7b8-4b88-446c-bd88-50e1902847d5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/PLAN_PORTAL/PP3.2_List of Enrollees_search</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a44cc28e-dded-4590-91b7-01c6ef89de42</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/PLAN_PORTAL/PP4.1_Enrollee_Submission_Management_page_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fd8a205d-892f-48f7-b60e-99146cdcfcf9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/PLAN_PORTAL/PP4.2_Rank_Coversheet_Page_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>df4b5fb6-dbbf-47d4-9ec6-a21e7f54255f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/PLAN_PORTAL/PP4.3_Coversheet_Summary_Page_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c499cef9-db19-419b-8313-6068223bed1e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/PLAN_PORTAL/PP5.1_Medical_Record_Coversheet_Page_Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2bf7e04a-6cda-413b-83d1-b67abc7a7ff8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/PLAN_PORTAL/PP5.2_No_Medical_Record_page</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4c4295e0-a163-4ede-944e-dcea0c221676</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/PLAN_PORTAL/PP6_Review_Coversheet_Page</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>544549b3-e297-4c47-bf96-856015ddcf91</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/PLAN_PORTAL/PP7_Review_Coversheet_Page_created</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>49ddf965-a6f0-4476-94cf-e061aff2163f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/PLAN_PORTAL/PP8.1_Plan_Library_Page</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>74d718ed-3e97-4241-9db2-2dd39afae13b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Page_Smoke_Test/PLAN_PORTAL/PP8.2_Team_Library_Page</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
